c8262f462d30055735bf46ba8cf704325650b6b1
LcYxT <33899471+LcYxT@users.noreply.github.com>
2021-02-01 21:18:34 +0800

    Add files via upload

--- script/map.js
+++ script/map.js
@@ -1,14 +1,14 @@
 var totalLocation = 3;
 var locationList = new Array(totalLocation);
 for (var i = 0; i < totalLocation; i++) {
-  locationList[i] = document.getElementById('Visited'.concat(i));
-  if (readCookie('location'.concat(i))) {
+  location_num = parseInt(i, 10)+1
+  locationList[i] = document.getElementById('Visited'.concat(location_num.toString());
+  if (readCookie('location'.concat(location_num.toString()))) {
   	locationList[i].innerHTML = 'Visited'
   }
 }
 
 document.getElementById('remove-cookie').addEventListener('click', function(){
-  eraseCookie_and_show('visitCount')
   eraseCookie('location1')
   eraseCookie('location2')
   eraseCookie('location3')
