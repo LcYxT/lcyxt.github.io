f0914a617eba1031be8cfa78b5a1b84b5e5859e3
LcYxT <33899471+LcYxT@users.noreply.github.com>
2021-02-01 16:30:51 +0800

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,7 +1,19 @@
+var visitCount_cookie = readCookie('visitCount');
+if (visitCount_cookie) {
+  var visitCount = parseInt(visitCount_cookie, 10);
+  document.getElementById('visitCount').innerHTML = "It is your ".concat(visitCount_cookie, " visit");
+  visitCount += 1;
+  createCookie('visitCount', visitCount.toString(), 1);
+}
+else {
+  document.getElementById('visitCount').innerHTML = "Hi newcomer!";
+  createCookie('visitCount', '1', 1);
+}
+
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false});
 
 scanner.addListener('scan', function (content) {
-  document.getElementById('result').innerHTML = content
+  document.getElementById('result').innerHTML = content;
 });
 
 var dropdown = document.getElementById('dropdown-menu');
@@ -10,41 +22,41 @@ Instascan.Camera.getCameras().then(function (cameras) {
   if (cameras.length > 0) {
     var i;
     var cameradropdown = new Array(cameras.length);
-    var cam_btn = new Array(cameras.length)
+    var cam_btn = new Array(cameras.length);
 
     for (i = 0; i < cameras.length; i++) {
-      cam_btn[i] = document.createElement('a')
-      cam_btn[i].id = "cam-btn".concat(i.toString())
-      cam_btn[i].className = "dropdown-item"
+      cam_btn[i] = document.createElement('a');
+      cam_btn[i].id = "cam-btn".concat(i.toString());
+      cam_btn[i].className = "dropdown-item";
       cam_btn[i].onclick = (function (i) {
         return function() {
           console.log(i)
-          myself_btn = document.getElementById('cam-btn'.concat(i.toString()))
+          var myself_btn = document.getElementById('cam-btn'.concat(i.toString()))
           myself_btn.className = 'dropdown-item disabled'
           myself_btn.setAttribute('aria-disabled', 'true')
           myself_btn.setAttribute('tabindex', '-1')
           for (var j = 0; j < cameras.length; j++){
             if (j != i){
-              document.getElementById('cam-btn'.concat(j.toString())).className = "dropdown-item"
-              document.getElementById('cam-btn'.concat(j.toString())).removeAttribute("aria-disabled")
-              document.getElementById('cam-btn'.concat(j.toString())).removeAttribute("tabindex")
-            }
-          }
+              document.getElementById('cam-btn'.concat(j.toString())).className = "dropdown-item";
+              document.getElementById('cam-btn'.concat(j.toString())).removeAttribute("aria-disabled");
+              document.getElementById('cam-btn'.concat(j.toString())).removeAttribute("tabindex");
+            };
+          };
           scanner.start(cameras[i]);
         };
       })(i);
-      cam_btn[i].href = "#"
-      cam_btn[i].appendChild(document.createTextNode(cameras[i].name))
+      cam_btn[i].href = "#";
+      cam_btn[i].appendChild(document.createTextNode(cameras[i].name));
       cameradropdown[i] = document.createElement('li').appendChild(cam_btn[i]);
       dropdown.appendChild(cameradropdown[i]);
     }
   }
 }).catch(function (e) {  // catching error from no camera or other error
-  var no_cam_label = document.createElement('a')
-  no_cam_label.className = 'dropdown-item disabled'
-  no_cam_label.setAttribute('aria-disabled', 'true')
-  no_cam_label.setAttribute('tabindex', '-1')
-  no_cam_label.appendChild(document.createTextNode('No camera found'))
+  var no_cam_label = document.createElement('a');
+  no_cam_label.className = 'dropdown-item disabled';
+  no_cam_label.setAttribute('aria-disabled', 'true');
+  no_cam_label.setAttribute('tabindex', '-1');
+  no_cam_label.appendChild(document.createTextNode('No camera found'));
   dropdown.appendChild(document.createElement('li').appendChild(no_cam_label));
   console.error('No camera found.');
   console.error(e);
